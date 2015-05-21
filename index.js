46423249ac57fb5213322daa2a4f6db8c5b5580b
Brandon J McKay <bmckay@rew.ca>
2015-05-21 16:30:23 -0700

    add initial import options

--- index.js
+++ index.js
@@ -2,5 +2,27 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-prism'
+  name: 'ember-prism',
+  included: function(app) {
+    //default theme name is 'default'
+    var options = app.options['ember-prism'] || { components: [] };
+
+    //import main stylesheet
+    app.import(app.bowerDirectory + '/prism/themes/prism.css');
+
+    //import theme based on options
+    if (options.theme){
+      app.import(app.bowerDirectory + '/prism/themes/prism-' + options.theme + '.css');
+    }
+
+    //import main javascript
+    app.import(app.bowerDirectory + '/prism/prism.js');
+
+    //import components
+    if (options.components){
+      options.components.forEach(function(component){
+        app.import(app.bowerDirectory + '/prism/components/prism-' + component + '.js');
+      });
+    }
+  }
 };
