58fa1759d3729eb3c956e14f296bc0319b5d69cb
Brandon J McKay <bmckay@rew.ca>
2015-05-22 15:54:49 -0700

    Add customizable theme, components, and plugins support

--- index.js
+++ index.js
@@ -2,5 +2,35 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-prism'
+  name: 'ember-prism',
+  included: function(app) {
+    //default theme name is 'default'
+    var options = app.options['ember-prism'] || { components: [], plugins: [] };
+
+    //import main stylesheet
+    app.import(app.bowerDirectory + '/prism/themes/prism.css');
+
+    //import main javascript
+    app.import(app.bowerDirectory + '/prism/prism.js');
+
+    //import theme based on options
+    if (options.theme){
+      app.import(app.bowerDirectory + '/prism/themes/prism-' + options.theme + '.css');
+    }
+
+    //import components
+    if (options.components){
+      options.components.forEach(function(component){
+        app.import(app.bowerDirectory + '/prism/components/prism-' + component + '.js');
+      });
+    }
+
+    //import plugins
+    if (options.plugins){
+      options.plugins.forEach(function(plugin){
+        app.import(app.bowerDirectory + '/prism/plugins/' + plugin + '/prism-' + plugin + '.js');
+        app.import(app.bowerDirectory + '/prism/plugins/' + plugin + '/prism-' + plugin + '.css');
+      });
+    }
+  }
 };
