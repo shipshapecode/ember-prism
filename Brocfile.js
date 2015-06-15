72b3ff5c52a9c24ca9311ebfd30c7a4149db980f
Justin Lau <justin@tclau.com>
2015-06-16 06:17:29 +0800

    Adds acceptance test for the `line-numbers` plugin.

--- Brocfile.js
+++ Brocfile.js
@@ -10,7 +10,14 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
   This Brocfile does *not* influence how the addon or the app using it
   behave. You most likely want to be modifying `./index.js` or app's Brocfile
 */
+var app = new EmberAddon({
 
-var app = new EmberAddon();
+  'ember-prism': {
+    plugins: [
+      'line-numbers',
+    ],
+  }
+
+});
 
 module.exports = app.toTree();
