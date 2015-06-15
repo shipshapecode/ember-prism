44745ef4600d4cf706c9290307e26fb46140ac07
Justin Lau <justin@tclau.com>
2015-06-16 04:21:29 +0800

    Fixes #4: Added the prism package; Fixes jshint errors; Made the dummy app more meaningful by actually displaying tags in HTML.

--- addon/components/code-block.js
+++ addon/components/code-block.js
@@ -1,4 +1,3 @@
-import Ember from 'ember';
 import CodeInline from './code-inline';
 
 export default CodeInline.extend({
--- addon/components/code-inline.js
+++ addon/components/code-inline.js
@@ -1,3 +1,5 @@
+/* global Prism */
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
