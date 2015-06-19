bfbb11eca5345927ac70d847a3e8abc8ba15484a
Twomey, Tim <tim.twomey@fmr.com>
2015-06-19 09:46:15 -0400

    Added 'none' filter, to have the option of providing custom css

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ module.exports = {
     //default theme name is 'default'
     var options = app.options['ember-prism'] || { components: [] };
 
-    //import main stylesheet
-    app.import(app.bowerDirectory + '/prism/themes/prism.css');
-
     //import theme based on options
     if (options.theme){
-      app.import(app.bowerDirectory + '/prism/themes/prism-' + options.theme + '.css');
+      // allow ability to specify no css if we want to provide our own
+      if (options.theme !== 'none'){
+        app.import(app.bowerDirectory + '/prism/themes/prism-' + options.theme + '.css');
+      } 
     } else {
+      // fall back to default theme
       app.import(app.bowerDirectory + '/prism/themes/prism.css');
     }
 
