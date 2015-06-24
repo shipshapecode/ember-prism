047378fd178bd396dc46da41814f56a1bb83f582
Brandon J McKay <admin@brandonjmckay.com>
2015-06-24 08:29:00 -0700

    Fix code classes to new syntax
     Clean up code-inline, and code-block modules

--- addon/components/code-block.js
+++ addon/components/code-block.js
@@ -1,13 +1,24 @@
-import CodeInline from './code-inline';
+/* global Prism */
 
-export default CodeInline.extend({
-  tagName:'pre',
-  classNames: ['codeblock'],
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'pre',
+  classNames: ['code-block'],
   classNameBindings: ['languageClass'],
 
-  inline:false,
+  inline: false,
+  language: 'markup',
+
+  languageClass: Ember.computed('language', function(){
+    return 'language-' + this.get('language');
+  }),
 
   getElement: function(){
     return this.$('[class*=language-]')[0];
+  },
+
+  didInsertElement: function(){
+    Prism.highlightElement(this.getElement());
   }
 });
--- addon/components/code-inline.js
+++ addon/components/code-inline.js
@@ -3,12 +3,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName:'code',
-  classNames: ['codeinline'],
+  tagName: 'code',
+  classNames: ['code-inline'],
   classNameBindings: ['languageClass'],
 
-  inline:true,
-  language:'markup',
+  inline: true,
+  language: 'markup',
+
   languageClass: Ember.computed('language', function(){
     return 'language-' + this.get('language');
   }),
