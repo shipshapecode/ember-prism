4350bfaeae44c31cfe538917ec04912636409d97
Sean Goresht <seangores@gmail.com>
2022-07-25 14:30:30 -0600

    feat: add @start to CodeBlock for custom line number start (#554)
    * feat: allow @start arg to override start line
    
    * doc: add documentation for overriding line numbers
    
    * doc(refactor): provide more generic example of custom `@start`

--- tests/integration/components/code-block-test.js
+++ tests/integration/components/code-block-test.js
@@ -54,4 +54,20 @@ module('Integration | Component | code-block', function (hooks) {
     assert.dom('code').hasText(code2);
     assert.dom('code > .tag').hasText('<p class="bar">');
   });
+
+  test('takes a @start param to apply on the <pre> element ', async function (assert) {
+    const code =
+      '<p>line 1</p>\n<p>line 2</p>\n<p>line 3</p>\n<p>line 4</p>\n<p>line 5</p>\n';
+    const lineStartNumber = 1000;
+    this.set('code', code);
+    this.set('start', lineStartNumber);
+    await render(hbs`
+      <CodeBlock @language="html" @code={{this.code}} @start={{this.start}} class="line-numbers"/>
+    `);
+
+    assert.dom('pre').hasAttribute('data-start', `${lineStartNumber}`);
+    assert.dom('.line-numbers').hasStyle({
+      'counter-reset': `linenumber ${lineStartNumber - 1}`,
+    });
+  });
 });
